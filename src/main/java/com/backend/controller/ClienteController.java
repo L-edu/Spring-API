871b230889d8394da264fdc1bcf1a0bebad139e4
L-edu <edurei75@gmail.com>
2023-11-28 21:36:56 -0300

    Projeto base

---
+++ src/main/java/com/backend/controller/ClienteController.java
@@ -0,0 +1,58 @@
+package com.backend.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.backend.model.Client;
+import com.backend.repository.ClientRepository;
+
+@RestController
+public class ClienteController {
+
+	@Autowired
+	private ClientRepository clientRepository;
+
+	// get all clients rest api
+	@GetMapping("/clients")
+	public List<Client> getAllClients() {
+		return clientRepository.findAll();
+
+	}
+
+	// get client by id rest api
+	@GetMapping("/clients/{id}")
+	public Client getClientById(@PathVariable Long id) {
+		return clientRepository.findById(id).get();
+	}
+
+	// create client rest api
+	@PostMapping("/clients")
+	public Client createClient(@RequestBody Client client) {
+		return clientRepository.save(client);
+	}
+
+	// update client rest api
+	@PutMapping("/clients/{id}")
+	public Client updateClient(@PathVariable Long id, @RequestBody Client clientDetails) {
+		Client client = clientRepository.findById(id).get();
+
+		client.setName(clientDetails.getName());
+		client.setEmail(clientDetails.getEmail());
+
+		return clientRepository.save(client);
+	}
+
+	// delete client rest api
+	@DeleteMapping("/clients/{id}")
+	public void deleteClient(@PathVariable Long id) {
+		clientRepository.deleteById(id);
+	}
+}
